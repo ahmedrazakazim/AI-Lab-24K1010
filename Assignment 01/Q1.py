3afdf786b2aa7ca00f73f40897b0da98185b26dc
Ahmed Raza <2004ahmadraza@gmail.com>
2026-01-25 23:58:11 +0500

    Update Q1.py

--- Assignment 01/Q1.py
+++ Assignment 01/Q1.py
@@ -1,1 +1,19 @@
 
+name = input("Enter your name: ")
+marks = int(input("Enter marks: "))
+print("Student Name: ", name)
+print("Marks: ", marks)
+
+if marks > 85:
+  grade = 'A'
+
+elif marks >= 70 and marks < 85:
+  grade = 'B'
+
+elif marks >= 50 and marks < 70:
+  grade = 'C'
+
+else :
+  grade = "Fail"
+
+print("Grade: ", grade)
