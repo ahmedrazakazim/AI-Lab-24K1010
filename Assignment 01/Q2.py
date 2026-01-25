07e7bc8c81a18aafdf4676f6713072cc9559e0f3
Ahmed Raza <2004ahmadraza@gmail.com>
2026-01-26 00:29:50 +0500

    Create Q2.py

---
+++ Assignment 01/Q2.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number: "))
+count = 2
+counter = 0
+print ("Even numbers: ")
+while (count <=n):
+  print( count , " ")
+  count = count + 2
+  counter = counter + 1
+
+print("Total even numbers: ", counter)
